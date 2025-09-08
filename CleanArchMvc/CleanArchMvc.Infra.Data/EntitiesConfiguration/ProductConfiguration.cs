544cb9accca245cf300128c8ea5ff9ef794b6a50
Daniel Rossi de Amorim <danielrossi5678@gmail.com>
2025-09-08 20:01:01 -0300

    feat: implementando a camada infrasctructure

---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Description).IsRequired().HasMaxLength(255);
+            builder.Property(p => p.Price).HasPrecision(10,2);
+            builder.HasOne(p => p.Category).WithMany(c => c.Products).HasForeignKey(p => p.CategoryId);
+        }
+    }
+
+}
